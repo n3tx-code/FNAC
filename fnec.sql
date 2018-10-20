94917e5dabc918a79e387b52482afb967644ed0d
n3tx <nils@vaede.com>
2018-10-20 22:49:54 +0200

    Correction db
    Correction erreur auto incrémentation id client

---
+++ fnec.sql
@@ -0,0 +1,423 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le :  jeu. 11 oct. 2018 à 10:17
+-- Version du serveur :  10.1.31-MariaDB
+-- Version de PHP :  7.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `fnec`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `address`
+--
+
+CREATE TABLE `address` (
+  `id` int(11) NOT NULL,
+  `client` int(11) NOT NULL,
+  `number` varchar(256) NOT NULL,
+  `street` varchar(256) NOT NULL,
+  `city` varchar(256) NOT NULL,
+  `zip_code` int(11) NOT NULL,
+  `description` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `category`
+--
+
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL,
+  `name` varchar(256) NOT NULL,
+  `parent` int(11) DEFAULT NULL,
+  `description` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `category`
+--
+
+INSERT INTO `category` (`id`, `name`, `parent`, `description`) VALUES
+(0, 'Informatique', NULL, 'L\'info c\'est bien'),
+(1, 'Jeux video', 0, 'JOUER'),
+(2, 'adadaad', 1, 'JOUER');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `client`
+--
+
+CREATE TABLE `client` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `fidelity_card` int(11) DEFAULT NULL,
+  `name` varchar(256) NOT NULL,
+  `first_name` varchar(256) NOT NULL,
+  `phone` int(11) NOT NULL,
+  `mail` varchar(256) NOT NULL,
+  `password` varchar(256) NOT NULL,
+   PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `command`
+--
+
+CREATE TABLE `command` (
+  `id` int(11) NOT NULL,
+  `client` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `price` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `delivery`
+--
+
+CREATE TABLE `delivery` (
+  `command` int(11) NOT NULL,
+  `address` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `status` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `fidelity_card`
+--
+
+CREATE TABLE `fidelity_card` (
+  `fc_number` int(11) NOT NULL,
+  `points` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `image`
+--
+
+CREATE TABLE `image` (
+  `reference` int(11) NOT NULL,
+  `src` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `opinion`
+--
+
+CREATE TABLE `opinion` (
+  `client` int(11) NOT NULL,
+  `reference` int(11) NOT NULL,
+  `grade` smallint(6) NOT NULL,
+  `comment` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `partner`
+--
+
+CREATE TABLE `partner` (
+  `id` int(11) NOT NULL,
+  `name` varchar(256) NOT NULL,
+  `description` text NOT NULL,
+  `website` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `partner`
+--
+
+INSERT INTO `partner` (`id`, `name`, `description`, `website`) VALUES
+(0, 'Nils', 'Il est beau', 'http://vaede.com');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `product`
+--
+
+CREATE TABLE `product` (
+  `reference` int(11) NOT NULL,
+  `command` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `promo`
+--
+
+CREATE TABLE `promo` (
+  `reference` int(11) NOT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL,
+  `percentage` smallint(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `reference`
+--
+
+CREATE TABLE `reference` (
+  `id` int(11) NOT NULL,
+  `category` int(11) NOT NULL,
+  `partner` int(11) DEFAULT NULL,
+  `ref_product` varchar(256) NOT NULL,
+  `name` varchar(256) NOT NULL,
+  `description` text NOT NULL,
+  `price` float NOT NULL,
+  `add_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `reference`
+--
+
+INSERT INTO `reference` (`id`, `category`, `partner`, `ref_product`, `name`, `description`, `price`, `add_date`) VALUES
+(0, 0, NULL, '1', 'AHAHAH', 'azda', 56, '2018-10-11 10:16:09');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `shop`
+--
+
+CREATE TABLE `shop` (
+  `identifiant` varchar(255) NOT NULL,
+  `street_number` int(11) NOT NULL,
+  `street` varchar(256) NOT NULL,
+  `city` varchar(256) NOT NULL,
+  `zip_code` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `stock`
+--
+
+CREATE TABLE `stock` (
+  `shop` varchar(255) NOT NULL,
+  `reference` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `address`
+--
+ALTER TABLE `address`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `client` (`client`);
+
+--
+-- Index pour la table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `parent` (`parent`);
+
+--
+-- Index pour la table `client`
+--
+ALTER TABLE `client`
+  ADD KEY `fidelity_card` (`fidelity_card`);
+
+--
+-- Index pour la table `command`
+--
+ALTER TABLE `command`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `client` (`client`);
+
+--
+-- Index pour la table `delivery`
+--
+ALTER TABLE `delivery`
+  ADD PRIMARY KEY (`command`),
+  ADD KEY `address` (`address`);
+
+--
+-- Index pour la table `fidelity_card`
+--
+ALTER TABLE `fidelity_card`
+  ADD PRIMARY KEY (`fc_number`);
+
+--
+-- Index pour la table `image`
+--
+ALTER TABLE `image`
+  ADD KEY `reference` (`reference`);
+
+--
+-- Index pour la table `opinion`
+--
+ALTER TABLE `opinion`
+  ADD PRIMARY KEY (`client`,`reference`),
+  ADD KEY `reference` (`reference`);
+
+--
+-- Index pour la table `partner`
+--
+ALTER TABLE `partner`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`reference`,`command`),
+  ADD KEY `command` (`command`);
+
+--
+-- Index pour la table `promo`
+--
+ALTER TABLE `promo`
+  ADD KEY `reference` (`reference`);
+
+--
+-- Index pour la table `reference`
+--
+ALTER TABLE `reference`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `category` (`category`),
+  ADD KEY `partner` (`partner`);
+
+--
+-- Index pour la table `shop`
+--
+ALTER TABLE `shop`
+  ADD PRIMARY KEY (`identifiant`);
+
+--
+-- Index pour la table `stock`
+--
+ALTER TABLE `stock`
+  ADD PRIMARY KEY (`shop`,`reference`),
+  ADD KEY `reference` (`reference`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `address`
+--
+ALTER TABLE `address`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `address`
+--
+ALTER TABLE `address`
+  ADD CONSTRAINT `address_ibfk_1` FOREIGN KEY (`client`) REFERENCES `client` (`id`);
+
+--
+-- Contraintes pour la table `category`
+--
+ALTER TABLE `category`
+  ADD CONSTRAINT `category_ibfk_1` FOREIGN KEY (`parent`) REFERENCES `category` (`id`);
+
+--
+-- Contraintes pour la table `client`
+--
+ALTER TABLE `client`
+  ADD CONSTRAINT `client_ibfk_1` FOREIGN KEY (`fidelity_card`) REFERENCES `fidelity_card` (`fc_number`);
+
+--
+-- Contraintes pour la table `command`
+--
+ALTER TABLE `command`
+  ADD CONSTRAINT `command_ibfk_1` FOREIGN KEY (`client`) REFERENCES `client` (`id`);
+
+--
+-- Contraintes pour la table `delivery`
+--
+ALTER TABLE `delivery`
+  ADD CONSTRAINT `delivery_ibfk_1` FOREIGN KEY (`command`) REFERENCES `command` (`id`),
+  ADD CONSTRAINT `delivery_ibfk_2` FOREIGN KEY (`command`) REFERENCES `command` (`id`),
+  ADD CONSTRAINT `delivery_ibfk_3` FOREIGN KEY (`command`) REFERENCES `command` (`id`),
+  ADD CONSTRAINT `delivery_ibfk_4` FOREIGN KEY (`command`) REFERENCES `command` (`id`),
+  ADD CONSTRAINT `delivery_ibfk_5` FOREIGN KEY (`address`) REFERENCES `address` (`id`);
+
+--
+-- Contraintes pour la table `image`
+--
+ALTER TABLE `image`
+  ADD CONSTRAINT `image_ibfk_1` FOREIGN KEY (`reference`) REFERENCES `reference` (`id`);
+
+--
+-- Contraintes pour la table `opinion`
+--
+ALTER TABLE `opinion`
+  ADD CONSTRAINT `opinion_ibfk_1` FOREIGN KEY (`client`) REFERENCES `client` (`id`),
+  ADD CONSTRAINT `opinion_ibfk_2` FOREIGN KEY (`reference`) REFERENCES `reference` (`id`);
+
+--
+-- Contraintes pour la table `product`
+--
+ALTER TABLE `product`
+  ADD CONSTRAINT `product_ibfk_1` FOREIGN KEY (`reference`) REFERENCES `reference` (`id`),
+  ADD CONSTRAINT `product_ibfk_2` FOREIGN KEY (`command`) REFERENCES `command` (`id`);
+
+--
+-- Contraintes pour la table `promo`
+--
+ALTER TABLE `promo`
+  ADD CONSTRAINT `promo_ibfk_1` FOREIGN KEY (`reference`) REFERENCES `reference` (`id`);
+
+--
+-- Contraintes pour la table `reference`
+--
+ALTER TABLE `reference`
+  ADD CONSTRAINT `reference_ibfk_1` FOREIGN KEY (`category`) REFERENCES `category` (`id`),
+  ADD CONSTRAINT `reference_ibfk_2` FOREIGN KEY (`partner`) REFERENCES `partner` (`id`);
+
+--
+-- Contraintes pour la table `stock`
+--
+ALTER TABLE `stock`
+  ADD CONSTRAINT `stock_ibfk_1` FOREIGN KEY (`reference`) REFERENCES `reference` (`id`),
+  ADD CONSTRAINT `stock_ibfk_2` FOREIGN KEY (`shop`) REFERENCES `shop` (`identifiant`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
