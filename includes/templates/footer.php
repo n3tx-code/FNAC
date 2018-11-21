53f9968870bb60f7a3b4c7f28e3556a2269f33f5
n3tx <nils@vaede.com>
2018-11-21 21:56:14 +0100

    footer

--- includes/templates/footer.php
+++ includes/templates/footer.php
@@ -1,7 +1,14 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: n3tx
- * Date: 08/10/2018
- * Time: 16:48
- */
+<div class="footer">
+    <div class="row">
+        <div class="col-md-2 col-md-offset-1">
+            <div class="text-left"><i class="far fa-envelope"></i> Contact</div>
+        </div>
+        <div class="col-md-2 col-md-offset-6">
+            <div class="text-right"><i class="fab fa-github"></i> Github</div>
+        </div>
+    </div>
+    <div class="row text-center">
+        Nils VAEDE & Jean Michel REMEUR<br>
+        <em>CNAM</em>
+    </div>
+</div>
