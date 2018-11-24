d5dd4ff690c3a3f4e158fd885baffb8f2d790897
n3tx <nils@vaede.com>
2018-11-24 17:47:48 +0100

    update footer

--- includes/templates/footer.php
+++ includes/templates/footer.php
@@ -1,10 +1,12 @@
 <div class="footer">
     <div class="row">
         <div class="col-md-2 col-md-offset-1">
-            <div class="text-left"><i class="far fa-envelope"></i> Contact</div>
+            <a class="footer-link" href="mailto:nils@vaede.com;jm.remeur@orange.fr ?subject=Félicitations pour votre 20/20&body=Bonjour, Je vous envoie ce mail pour vous notifiez que vous avez reçu 20/20.">
+                <div class="text-left"><i class="far fa-envelope"></i> Contact</div>
+            </a>
         </div>
         <div class="col-md-2 col-md-offset-6">
-            <div class="text-right"><i class="fab fa-github"></i> Github</div>
+            <a class="footer-link" href="https://github.com/n3tx-code/FNAC"><div class="text-right"><i class="fab fa-github"></i> Github</div></a>
         </div>
     </div>
     <div class="row text-center">
